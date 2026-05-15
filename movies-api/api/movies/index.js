37ce8b6164050018adf9e36c81af27b5caf687ef
Maria <20106500@mail.wit.ie>
2026-05-15 15:09:37 +0100

    Added movies router; get movies from TMDB

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -5,11 +5,10 @@ import express from 'express';
 const router = express.Router();
 
 // movie routes to be added
+
+export default router;
+
 router.get('/discover', asyncHandler(async (req, res) => {
     const discoverMovies = await getMovies();
     res.status(200).json(discoverMovies);
 }));
-
-
-
-export default router;
